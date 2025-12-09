bf1d6a264f54ecc701ec7b9a4ff4afeb23445c04
Samith R Gowda <141106865+Samithsr@users.noreply.github.com>
2025-12-09 17:28:44 +0530

    backend basic server configurationass

--- config/db.js
+++ config/db.js
@@ -1,2 +1,5 @@
 import mongoose from "mongoose";
 
+export const connectDB = async () => {
+    await mongoose.connect('mongodb+srv://rgowdasamith_db_user:Sami0821%40@cluster0.aqyf6o9.mongodb.net/food-del').then(()=>console.log("DB Connected"));
+}
--- server.js
+++ server.js
@@ -1,25 +1,37 @@
 import express from "express";
 import cors from "cors";
-
+import { config } from "dotenv";
+import { connectDB } from "./config/db.js";
 
 // app config
 const app = express();
 const port = 4000;
 
-// middleware
-
+// middeleware
 app.use(express.json());
 app.use(cors());
 
 
-app.get("/",(req,res)=>{
-    res.send("API working")
+//db connection
+
+connectDB();
+
+app.get("/", (req, res) => {
+  res.send("API WORKING");
 });
 
 
 app.listen(port,()=>{
-    console.log(`server porting on http://localhost:${port}`);
-    
+    console.log(`Serving port on http://localhost:${port}`);
 })
+
+
+
+
+
+
+
+
+
 
 // mongodb+srv://rgowdasamith_db_user:Sami0821@@cluster0.aqyf6o9.mongodb.net/?
